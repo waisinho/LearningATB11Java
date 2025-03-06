a6ddcb5e7b81cbaf85b4706b924d8ef5a97323b5
waisinho <wais.ghaussi1@gmail.com>
2025-03-05 21:53:24 -0500

    testing to see if we can add this to git

---
+++ src/ex_01_Java/Lab001HelloWorld.java
@@ -0,0 +1,7 @@
+package ex_01_Java;
+
+public class Lab001HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is a test to see if we can add this to git");
+    }
+}
