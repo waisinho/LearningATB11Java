0f6387e8eafebf9aaf190a00c9329da555184bee
waisinho <wais.ghaussi1@gmail.com>
2025-03-06 20:34:38 -0500

    testing to see if we can add this to git

--- src/ex_01_Java/Lab001HelloWorld.java
+++ src/ex_01_Java/Lab001HelloWorld.java
@@ -2,6 +2,6 @@ package ex_01_Java;
 
 public class Lab001HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is a test to see if we can add this to git");
+        System.out.println("This is a test to see if we can add this to git\n");
     }
 }
