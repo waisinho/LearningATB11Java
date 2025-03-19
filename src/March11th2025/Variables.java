f87663edf55c36274d7989076e9223f04b682546
waisinho <wais.ghaussi1@gmail.com>
2025-03-19 11:51:02 -0400

    testing to see if we can add this to git

---
+++ src/March11th2025/Variables.java
@@ -0,0 +1,17 @@
+package March11th2025;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte b = 10;
+        //bye -> data type - type of container - stores only integer value but limited to max and min size
+        int age = 76;
+        age = age + 1;
+        age = 76;
+        System.out.println(age);
+        double Aage = 225.20d;
+
+
+
+    }
+
+}
